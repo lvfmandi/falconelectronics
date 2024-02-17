81ccc78f2fefd265d635b4b4932314a35dce4911
lvfmandi <lvfmandi@gmail.com>
2024-02-17 11:38:26 +0300

    This will add the 5000 error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
 interface ErrorProps {
   error: Error;
   reset: () => void;
@@ -7,10 +11,20 @@ interface ErrorProps {
 
 export default function Error({ error, reset }: ErrorProps) {
   return (
-    <div className="container">
-      <span>Error: {error.message}</span>
-      <br />
-      <button onClick={reset}>Try Again</button>
-    </div>
+    <main className="container font-inter min-h-[70vh] grid place-items-center justify-items-center">
+      <div className="grid gap-4 justify-items-center">
+        <h4 className="font-light">An error occured</h4>
+        <p>Try again or navigate to the home page</p>
+        <div className="flex gap-4">
+          <Button onClick={reset}>Try Again</Button>
+          <Link
+            className={`${cn(buttonVariants({ variant: "secondary" }))}`}
+            href={"/"}
+          >
+            Go back Home
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
